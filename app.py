92a7d70765dcf49f298cd9a63c7356a8b6228d63
Rembert Daems <rembertdaems@gmail.com>
2023-12-27 12:59:00 +0100

    demo works

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 from io import BytesIO
-from PIL import Image, ImageDraw
-import numpy as np
+from PIL import Image
 import base64
 from painter import Painter
+import threading
+import time
+import numpy as np
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 buffer = []
-processing = False
+processing_lock = threading.Lock()
 painter = Painter()
+painter.encode_text('Pieter Breughel the Elder')
 
 def process_strokes():
-    global buffer, processing
-    processing = True
-    strokes, buffer = buffer, []
-    paint(strokes)
+    global buffer, processing_lock
+    if not processing_lock.locked() and len(buffer) > 0:
+        with processing_lock:
+            strokes = buffer.copy()
+            buffer.clear()
+            paint(strokes)
 
-    if len(buffer) > 0:
         process_strokes()
-    else:
-        processing = False
 
 def paint(strokes):
     global painter
-    canvas = painter.draw(strokes)
+    canvas = painter.paint(strokes)
     image = Image.fromarray(canvas)
     byte_array = BytesIO()
     image.save(byte_array, format='PNG')
     byte_array.seek(0)
     data = base64.b64encode(byte_array.getvalue()).decode('utf-8')
-    emit('image_updated', data)
+    socketio.emit('image_updated', data)
 
 @socketio.on('update_strokes')
 def update(new_stroke):
-    global buffer, processing
+    global buffer
     buffer.append(new_stroke)
-
-    if not processing:
-        process_strokes()
+    socketio.start_background_task(process_strokes)
 
 @app.route('/')
 def index():
--- painter.py
+++ painter.py
@@ -10,18 +10,20 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class Painter:
     def __init__(self):
-        self.vae = AutoencoderKL.from_pretrained('CompVis/stable-diffusion-v1-4', subfolder='vae').to(device)
+        self.vae = AutoencoderKL.from_pretrained('runwayml/stable-diffusion-v1-5', subfolder='vae').to(device)
         self.tokenizer = CLIPTokenizer.from_pretrained('openai/clip-vit-large-patch14')
         self.text_encoder = CLIPTextModel.from_pretrained('openai/clip-vit-large-patch14').to(device)
-        self.unet = UNet2DConditionModel.from_pretrained('CompVis/stable-diffusion-v1-4', subfolder='unet').to(device)
-        self.scheduler = DDIMScheduler.from_pretrained('CompVis/stable-diffusion-v1-4', subfolder='scheduler')
+        self.unet = UNet2DConditionModel.from_pretrained('runwayml/stable-diffusion-v1-5', subfolder='unet').to(device)
+        self.scheduler = DDIMScheduler.from_pretrained('runwayml/stable-diffusion-v1-5', subfolder='scheduler')
 
         self.latents = torch.randn((1, 4, 512 // 8, 512 // 8)).to(device)
         self.stroke_map = torch.zeros((512 // 8, 512 // 8)).to(device)
-        self.patch_radius = 5
+        self.patch_radius = 10
         self.scheduler.set_timesteps(30)
         self.guidance_scale = 7.5
+
         self.latents = self.latents * self.scheduler.init_noise_sigma
+        self.pred_latents = torch.zeros_like(self.latents)
 
     def encode_text(self, text):
         text_input = self.tokenizer([text], padding='max_length', max_length=self.tokenizer.model_max_length, truncation=True, return_tensors='pt')
@@ -33,9 +35,9 @@ class Painter:
             uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(device))[0]
         self.text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
 
-    def latents_to_image(self):
+    def latents_to_image(self, latents):
         with torch.no_grad():
-            image = self.vae.decode(self.latents / .18215).sample
+            image = self.vae.decode(latents / .18215).sample
         image = (image / 2 + 0.5)
         image = image.cpu().permute(0, 2, 3, 1).float().numpy()[0]
         image = (image * 255).clip(0, 255).astype(np.uint8)
@@ -44,12 +46,12 @@ class Painter:
     def paint(self, strokes):
         patch = torch.zeros_like(self.stroke_map)
         for stroke in strokes:
-            rr, cc = disk((stroke['x'], stroke['y']), self.patch_radius, shape=patch.shape)
+            rr, cc = disk((stroke['y'] // 8, stroke['x'] // 8), self.patch_radius, shape=patch.shape)
             patch[rr, cc] = 1
 
         estimated_num_steps = self.stroke_map[patch == 1].mean()
         estimated_num_steps = 0 if torch.isnan(estimated_num_steps) else int(estimated_num_steps)
-        estimated_num_steps = max(estimated_num_steps, len(self.scheduler.timesteps) - 1)
+        estimated_num_steps = min(estimated_num_steps, len(self.scheduler.timesteps) - 1)
         t = self.scheduler.timesteps[estimated_num_steps]
 
         latent_model_input = torch.cat([self.latents, self.latents])
@@ -59,10 +61,15 @@ class Painter:
         noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
         noise_pred = noise_pred_uncond + self.guidance_scale * (noise_pred_text - noise_pred_uncond)
 
-        noise_pred = patch[None, None, :, :] * noise_pred
-        max_channel = torch.argmax(noise_pred.abs().sum(dim=(0, 2, 3)))
-        masked_noise_pred = torch.zeros_like(noise_pred)
-        masked_noise_pred[:, max_channel, :, :] = noise_pred[:, max_channel, :, :]
+        mask = patch[None, None, :, :]
+        # noise_pred_patch = patch[None, None, :, :] * noise_pred
+        # max_channel = torch.argmax((noise_pred_patch ** 2).sum(dim=(0, 2, 3)))
+        # mask = torch.zeros_like(self.latents)
+        # mask[:, max_channel, :, :] = patch[None, :, :]
 
-        self.latents = self.scheduler.step(masked_noise_pred, t, self.latents).prev_sample
+        step = self.scheduler.step(noise_pred, t, self.latents)
+        self.latents = mask * step.prev_sample + (1 - mask) * self.latents
+        self.pred_latents = mask * step.pred_original_sample + (1 - mask) * self.pred_latents
+
         self.stroke_map += patch
+        return self.latents_to_image(self.pred_latents)
